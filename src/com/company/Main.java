15c66f5b570f0fce3f1a3e6155be27ae9c8421db
Şahin <sahincakmak7@gmail.com>
2022-04-03 16:02:14 +0300

    commit1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,52 +17,52 @@ public class Main {
             System.out.println("Hatalı veri girdiniz.");
             return;
         }
-            System.out.print("Lüften yaşınızı giriniz :");
-            age = input.nextInt();
+        System.out.print("Lüften yaşınızı giriniz :");
+        age = input.nextInt();
 
-            if (age < 0) {
-                System.out.println("Hatalı veri girdiniz.");
-                return;
-            }
-
-            System.out.print("Lüften yolculuk tipini giriniz :");
-            System.out.println("\n1-Tek yön\n2-Gidiş-dönüş");
-            direction = input.nextInt();
-
-            if (direction == 1) {
-                if (0 < age && age <= 12) {
-                    totalPrice = ((kmPrice * km) * 0.5);
-                    System.out.println("Bilet tutarı :" + totalPrice+" TL");
-                }
-                if (age > 12 && age < 24) {
-                    totalPrice = ((kmPrice * km) * 0.1);
-                    System.out.println("Bilet tutarı :" + totalPrice+" TL");
-                }
-                if (age > 65) {
-                    totalPrice = ((kmPrice * km) * 0.35);
-                    System.out.println("Bilet tutarı :" + totalPrice+" TL");
-                }
-
-
-            } else if (direction == 2) {
-                if (age <= 12) {
-                    totalPrice = (((kmPrice * km) * 0.5) * 2);
-                    System.out.println("Bilet tutarı :" + totalPrice+" TL");
-                }
-                if (age > 12 && age < 24) {
-                    totalPrice = (((kmPrice * km) * 0.1) * 2);
-                    System.out.println("Bilet tutarı :" + totalPrice+" TL");
-                }
-                if (age > 65) {
-                    totalPrice = (((kmPrice * km) * 0.35) * 2);
-                    System.out.println("Bilet tutarı :" + totalPrice+" TL");
-                }
-
-
-            } else {
-                System.out.println("Hatalı veri girdiniz.");
-            }
-
-
+        if (age < 0) {
+            System.out.println("Hatalı veri girdiniz.");
+            return;
         }
+
+        System.out.print("Lüften yolculuk tipini giriniz :");
+        System.out.println("\n1-Tek yön\n2-Gidiş-dönüş");
+        direction = input.nextInt();
+
+        if (direction == 1) {
+            if (0 < age && age <= 12) {
+                totalPrice = ((kmPrice * km) * 0.5);
+                System.out.println("Bilet tutarı :" + totalPrice + " TL");
+            }
+            if (age > 12 && age < 24) {
+                totalPrice = ((kmPrice * km) * 0.1);
+                System.out.println("Bilet tutarı :" + totalPrice + " TL");
+            }
+            if (age > 65) {
+                totalPrice = ((kmPrice * km) * 0.35);
+                System.out.println("Bilet tutarı :" + totalPrice + " TL");
+            }
+
+
+        } else if (direction == 2) {
+            if (age <= 12) {
+                totalPrice = (((kmPrice * km) * 0.5) * 2);
+                System.out.println("Bilet tutarı :" + totalPrice + " TL");
+            }
+            if (age > 12 && age < 24) {
+                totalPrice = (((kmPrice * km) * 0.1) * 2);
+                System.out.println("Bilet tutarı :" + totalPrice + " TL");
+            }
+            if (age > 65) {
+                totalPrice = (((kmPrice * km) * 0.35) * 2);
+                System.out.println("Bilet tutarı :" + totalPrice + " TL");
+            }
+
+
+        } else {
+            System.out.println("Hatalı veri girdiniz.");
+        }
+
+
+    }
 }
